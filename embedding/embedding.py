0b4dd4b9cf2a82045b1ead3828fb5cd2b51fd858
h1226933031 <125848749+h1226933031@users.noreply.github.com>
2023-03-16 17:37:21 +0800

    Add files via upload

---
+++ embedding/embedding.py
@@ -0,0 +1,47 @@
+import gensim
+import pickle
+import numpy
+import numpy as np
+
+print(gensim.__version__)
+
+class SciCiteEmbedding:
+    def __init__(self, glove=False, word2vec=False, elmo=False, bert=False):
+        self.word2vec =word2vec
+        self.glove = glove
+        self.elmo = elmo
+        self.bert = bert
+        self.ndim = None
+        self.ndim = []
+
+    def embed(self, X_train):
+        embed_list = []
+        if self.word2vec:
+            pass
+
+        elif self.glove:
+            model_path = './glove2word2vec_model.sav'  # dimension=100
+            glove = pickle.load(open(model_path, 'rb'))
+            for token_list in X_train:
+                np_list = [glove[token].reshape(1, -1) for token in token_list]
+                embed_list.append(np.concatenate(np_list))
+            return embed_list
+
+        elif self.elmo:
+            pass
+
+        elif self.bert:
+            pass
+
+    def seq_padding(self, X_train): # no need!!!
+        embed_list = self.embed(X_train)
+        max_length =
+        pass
+
+    def save_npy(self, X_train):
+        pass
+
+
+test = [['ok',',','fine','i','will','check','it','later','.'],
+           ['love','you','!'],
+           ['i', 'think','data','preprocessing','is','so','complicated','.']]
